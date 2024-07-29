b48375c8cda3053ed554d4a06af78b311342cab2
Shivam150758 <ssingh@fossil.com>
2024-07-29 09:39:59 +0530

    Redirection Link Correction

--- src/service/API/api-calling.service.ts
+++ src/service/API/api-calling.service.ts
@@ -2,7 +2,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
